77fe1329d8cb5762c8b1f3890870ab00132b6d04
Kali Chen <huiyichen19830120@gmail.com>
2019-07-14 17:58:29 -0700

    20190715 ========== 1. moved out database 2. Translator1, 2

--- Main.py
+++ Main.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from Translator import Translator
+from Translator import Translator1
+from Translator import Translator2
 
-T = Translator()
+T1 = Translator1()
+T2 = Translator2()
 
 while True:
     word = input("Enter a word: ")
     if word == "-1":
         break
-    word = T.TranslateSentense(word)
-    print(word)
+#    word = T.TranslateSentense(word)
+#    print(word)
+    word1 = T1.TranslateSentense(word)
+    word2 = T2.TranslateSentense(word)
+    print(word1)
+    print(word2)
