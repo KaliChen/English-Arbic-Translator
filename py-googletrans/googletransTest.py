b051a687e275e1fe90ecb877d3c054a019fc7161
KaliChen <huiyichen19830120@gmail.com>
2019-08-19 02:29:39 -0700

    20190819 ========= modified:   py-googletrans/googletransTest.py

--- py-googletrans/googletransTest.py
+++ py-googletrans/googletransTest.py
@@ -2,23 +2,23 @@
 from googletrans import Translator
 import linecache
 
-#1)Basic Usage
-#  If source language is not given, google translate attempts to detect the source language.
-translator = Translator()
-print(translator.translate('안녕하세요.'))
-# <Translated src=ko dest=en text=Good evening. pronunciation=Good evening.>
-print(translator.translate('안녕하세요.', dest='ja'))
-# <Translated src=ko dest=ja text=こんにちは。 pronunciation=Kon'nichiwa.>
-print(translator.translate('veritas lux mea', src='la'))
-# <Translated src=la dest=en text=The truth is my light pronunciation=The truth is my light>
-print(translator.translate('Hello', dest = 'ar'))
+##1)Basic Usage
+##  If source language is not given, google translate attempts to detect the source language.
+#translator = Translator()
+#print(translator.translate('안녕하세요.'))
+## <Translated src=ko dest=en text=Good evening. pronunciation=Good evening.>
+#print(translator.translate('안녕하세요.', dest='ja'))
+## <Translated src=ko dest=ja text=こんにちは。 pronunciation=Kon'nichiwa.>
+#print(translator.translate('veritas lux mea', src='la'))
+## <Translated src=la dest=en text=The truth is my light pronunciation=The truth is my light>
+#print(translator.translate('Hello', dest = 'ar'))
 
-#2)Customize service URL
-#  You can use another google translate domain for translation. If multiple URLs are provided it then randomly chooses a domain.
+##2)Customize service URL
+##  You can use another google translate domain for translation. If multiple URLs are provided it then randomly chooses a domain.
 
 translator = Translator(service_urls=[
     'translate.google.com',
-    'translate.google.co.kr',
+#    'translate.google.co.kr',
 ])
 
 #3)Advanced Usage (Bulk)
@@ -32,32 +32,36 @@ for translation in translations:
 # jumps over  ->  이상 점프
 # the lazy dog  ->  게으른 개
 
-#4)Language detection
-#  The detect method, as its name implies, identifies the language used in a given sentence.
-translator.detect('이 문장은 한글로 쓰여졌습니다.')
-# <Detected lang=ko confidence=0.27041003>
-translator.detect('この文章は日本語で書かれました。')
-# <Detected lang=ja confidence=0.64889508>
-translator.detect('This sentence is written in English.')
-# <Detected lang=en confidence=0.22348526>
-translator.detect('Tiu frazo estas skribita en Esperanto.')
-# <Detected lang=eo confidence=0.10538048>
+##4)Language detection
+##  The detect method, as its name implies, identifies the language used in a given sentence.
+#translator.detect('이 문장은 한글로 쓰여졌습니다.')
+## <Detected lang=ko confidence=0.27041003>
+#translator.detect('この文章は日本語で書かれました。')
+## <Detected lang=ja confidence=0.64889508>
+#translator.detect('This sentence is written in English.')
+## <Detected lang=en confidence=0.22348526>
+#translator.detect('Tiu frazo estas skribita en Esperanto.')
+## <Detected lang=eo confidence=0.10538048>
 
 
 
 f = open('arabic-wordlist-1.6.txt','r')
 #print(f.read())
 
-#line_nu = 0#计数器
+line_nu = 0#计数器
 for line in f:#一行行的把数据从硬盘加载到内存里读出来
-    print(line.strip())
-    print(translator.translate(line.strip(), src='ar', dest = 'en'))
-#    if line_nu <500 :#读取前五行
-#        print(translator.translate(line.strip(), src='ar', dest = 'en'))
-#        print(line.strip())
-#        line_nu += 1
-#    else:
-#        break
+    #print(line_nu)
+    translations = translator.translate( line.strip(), dest='en')
+    print(str(line_nu) + line.strip() + translations.text)
+
+##    print(translator.translate(line.strip(), src='ar', dest = 'en'))
+###    if line_nu <600 :#读取前600行
+###        print(translator.translate(line.strip(), src='ar', dest = 'en'))
+###        print(line.strip())        
+###        print(line_nu)
+###    else:
+###        break
+    line_nu += 1
 
 
 
